3b9e1a9663541df44ef57123edded07a9c194b95
DG <thsdimaker@gmail.com>
2021-04-15 14:28:02 +0900

    20210415

---
+++ mytest.java
@@ -0,0 +1,29 @@
+class Main {  
+    public static void main(String args[]) {
+      Thread thread = new DigitThread();
+      thread.start();
+      for(char ch = 'A'; ch<='Z'; ch++){
+        System.out.println(ch);
+        try{
+          Thread.sleep(1000);
+        }catch(InterruptedException e){
+          System.out.println(e.getMessage());
+        }
+      }
+    } 
+  }
+  
+  class DigitThread extends Thread{
+    public void run(){
+      for(int cnt=0; cnt<10; cnt++){
+        System.out.print(cnt);
+        try{
+          Thread.sleep(1000);
+        }catch(InterruptedException e){
+          System.out.println(e.getMessage());
+        }
+      }
+    }
+  
+  }
+  
